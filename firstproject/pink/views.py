b58f1d6dfdfcf7e689ef9a23f1f6a860bd578ae9
Edralyn Rose Bibera <edralynrosebibera@gmail.com>
2025-09-21 19:57:38 +0800

    feature(homeapp): add pink landing page with button to pink app

--- firstproject/pink/views.py
+++ firstproject/pink/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def pink_page(request):
-    return HttpResponse("<h1>This is the Pink Page!</h1>")
+    return render(request, "pink.html")
